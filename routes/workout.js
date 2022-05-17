0bd762be7ebf566a139e52eca4aa076db124fd78
Kris Gutmann <astraete.mail@gmail.com>
2022-05-17 23:45:04 +0300

    API test

--- routes/workout.js
+++ routes/workout.js
@@ -2,21 +2,27 @@ const workout = require("express").Router();
 const ObjectId = require('mongodb').ObjectId;
 const { connectToDatabase } = require("../db/conn");
 
-workout.get("/workout/:id", async function (req, res) {
-  try {
-    let { db } = await connectToDatabase();
-    let myquery = { _id: ObjectId(req.params.id) };
-    let workout = await db
-        .collection('workouts')
-        .findOne(myquery);
+workout.get("/workout", async function (req, res) {
+  const { workoutId } = req.query;
+  if (workoutId) {
+    try {
+      let { db } = await connectToDatabase();
+      let myquery = { _id: ObjectId(workoutId) };
+      let workout = await db
+          .collection('workouts')
+          .findOne(myquery);
 
-    return res.json(workout);
-  } catch (error) {
-    return res.json({
-        message: new Error(error).message,
-        success: false,
-    });
+      return res.json(workout);
+    } catch (error) {
+      return res.json({
+          message: new Error(error).message,
+          success: false,
+      });
+    }
+  } else {
+    res.send("No workout ID");
   }
 });
 
 module.exports = workout;
+
