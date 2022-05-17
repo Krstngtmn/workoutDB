35557adbaefc8191f9f42012390e077adb94ab74
Kris Gutmann <astraete.mail@gmail.com>
2022-05-18 01:37:50 +0300

    API test

--- routes/addworkout.js
+++ routes/addworkout.js
@@ -1,32 +1,48 @@
 const addworkout = require("express").Router();
 const { connectToDatabase } = require("../db/conn");
 
+const getCircularReplacer = () => {
+  const seen = new WeakSet();
+  return (key, value) => {
+    if (typeof value === "object" && value !== null) {
+      if (seen.has(value)) {
+        return;
+      }
+      seen.add(value);
+    }
+    return value;
+  };
+};
+
+
+
 addworkout.post("/addworkout", async function (req, res) {
   try {
-    let { db } = await connectToDatabase();
+    // let { db } = await connectToDatabase();
 
     return res.json({
-      message: req.query,
+      message: req,
+      test: JSON.stringify(req, getCircularReplacer()),
       success: true,
     });
 
-    const requestJSON = JSON.parse(req.body);
+    // const requestJSON = JSON.parse(req.body);
 
-    let workout = {
-      user_id: requestJSON.user_id,
-      workout_name: requestJSON.workout_name,
-      exercises: requestJSON.exercises,
-      finished_workouts: requestJSON.finished_workouts,
-    };
+    // let workout = {
+    //   user_id: requestJSON.user_id,
+    //   workout_name: requestJSON.workout_name,
+    //   exercises: requestJSON.exercises,
+    //   finished_workouts: requestJSON.finished_workouts,
+    // };
     
-    await db
-      .collection('workouts')
-      .insertOne(workout);
+    // await db
+    //   .collection('workouts')
+    //   .insertOne(workout);
 
-    return res.json({
-      message: 'Post added successfully',
-      success: true,
-    });
+    // return res.json({
+    //   message: 'Post added successfully',
+    //   success: true,
+    // });
   } catch (error) {
     return res.json({
         message: new Error(error).message,
