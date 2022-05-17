65ed730773253a63593209decbe226fcf29f3ced
Kris Gutmann <astraete.mail@gmail.com>
2022-05-17 21:39:47 +0300

    API test

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
-require("dotenv").config({ path: "./config.env" });
-const port = process.env.PORT || 5001;
-app.use(cors());
-app.use(express.json());
-app.use(require("./api/workouts"));
-const dbo = require("./db/conn");
+// const express = require("express");
+// const app = express();
+// const cors = require("cors");
+// require("dotenv").config({ path: "./config.env" });
+// const port = process.env.PORT || 5001;
+// app.use(cors());
+// app.use(express.json());
+// app.use(require("./api/workouts"));
+// const dbo = require("./db/conn");
 
-app.listen(port, () => {
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
-  });
-  console.log(`Server is running on port ${port}`);
+// app.listen(port, () => {
+//   dbo.connectToServer(function (err) {
+//     if (err) console.error(err);
+//   });
+//   console.log(`Server is running on port ${port}`);
+// });
+
+// module.exports = app;
+
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Express on Vercel");
 });
 
+app.listen(5000, () => {
+  console.log("Running on port 5000.");
+});
+
+// Export the Express API
 module.exports = app;
