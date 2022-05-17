211c8ec1c74ab1c9ecab339f8a3535cc26c22a7d
Kris Gutmann <astraete.mail@gmail.com>
2022-05-18 00:22:19 +0300

    API test

--- routes/workout-add.js
+++ routes/workout-add.js
@@ -5,11 +5,13 @@ newWorkout.post("/workout-add", async function (req, res) {
   try {
     let { db } = await connectToDatabase();
 
+    const { user_id, workout_name, exercises, finished_workouts } = req.body;
+
     let workout = {
-      user_id: req.body.user_id,
-      workout_name: req.body.workout_name,
-      exercises: req.body.exercises,
-      finished_workouts: req.body.finished_workouts,
+      user_id: user_id,
+      workout_name: workout_name,
+      exercises: exercises,
+      finished_workouts: finished_workouts,
     };
     
     let newWorkout = await db
@@ -19,7 +21,9 @@ newWorkout.post("/workout-add", async function (req, res) {
         response.json(res);
       });
 
-    return res.json(newWorkout);
+    res.send("Something done");
+
+    // return res.json(newWorkout);
   } catch (error) {
     return res.json({
         message: new Error(error).message,
